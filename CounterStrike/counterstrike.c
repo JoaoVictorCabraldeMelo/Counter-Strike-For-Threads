0b39cad8605238a24ec96c78371b1e134ba582e9
Joao Victor Cabral de Melo <JoaoVictorCabraldeMelo>
2020-11-18 16:24:30 -0300

    :sparkles: Bomba adicionada ao jogo

--- CounterStrike/counterstrike.c
+++ CounterStrike/counterstrike.c
@@ -9,10 +9,9 @@
 #define CONTRATERRORISTA 5
 #define NRO_DE_JOGADORES TERRORISTA + CONTRATERRORISTA
 #define ANDANDO 0
-#define PLANTANDO 2
-#define DESARMANDO 3
+#define BOMBA_PLANTADA 2
+#define BOMBA_DESARMADA 3
 #define MORTO 6
-#define TODOS_DO_TIME_MORRERAM 4
 #define ACHA_INIMIGO(upper, lower) ((rand() % (upper - lower + 1)) + lower)
 #define CALCULA_DANO (rand() % 101)
 #define MUDA_ESTADO(n, state) (estados[n].estado = state)
@@ -29,10 +28,14 @@ int contador_terroristas = TERRORISTA;
 int contador_contraterrorista = CONTRATERRORISTA;
 int contador_rodadas_terrorista = 0;
 int contador_rodadas_contraterrorista = 0;
+int estado_bomba = 0;
 
 pthread_mutex_t lock_player = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t lock_estado = PTHREAD_MUTEX_INITIALIZER;
 pthread_mutex_t lock_contador = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t lock_terrorista = PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t lock_contraterrorista = PTHREAD_MUTEX_INITIALIZER;
+
 
 void *terrorista(void *arg);
 void *contraterrorista(void *arg);
@@ -40,6 +43,8 @@ void procura_combate(int n);
 void mostra_dano(int jogador, int adversario, int dano);
 void reseta_rodada();
 void vencedor_da_rodada(int nro_terroristas, int nro_contraterroristas);
+void planta_bomba(int id_terrorista);
+void desarma_bomba(int id_contraterrorista);
 
 int main()
 {
@@ -85,6 +90,12 @@ void *terrorista(void *arg)
       printf("Terrorista %d\n", n);
       procura_combate(n);
     }
+    if (rand() % 15 == 7)
+    {
+      pthread_mutex_lock(&lock_terrorista);
+      planta_bomba(n);
+      pthread_mutex_unlock(&lock_terrorista);
+    }
     if (contador_contraterrorista == 0 || contador_terroristas == 0)
     {
       reseta_rodada();
@@ -110,6 +121,12 @@ void *contraterrorista(void *arg)
     {
       printf("Contra-Terrorista %d\n", n);
       procura_combate(n);
+    }
+    if (estado_bomba == BOMBA_PLANTADA && contador_terroristas == 0 && contador_contraterrorista > 0)
+    {
+      pthread_mutex_lock(&lock_contraterrorista);
+      desarma_bomba(n);
+      pthread_mutex_unlock(&lock_contraterrorista);
     }
     if (contador_contraterrorista == 0 || contador_contraterrorista == 0)
     {
@@ -209,6 +226,7 @@ void reseta_rodada()
   vencedor_da_rodada(contador_terroristas, contador_contraterrorista);
   contador_contraterrorista = CONTRATERRORISTA;
   contador_terroristas = TERRORISTA;
+  estado_bomba = 0;
   pthread_mutex_unlock(&lock_contador);
 }
 
@@ -224,4 +242,18 @@ void vencedor_da_rodada(int nro_terroristas, int nro_contraterrorista)
     contador_rodadas_contraterrorista += 1;
     printf("\n\n\nContra-Terroristas Venceram\n Rodadas Terroristas: %d \tRodadas Contra-Terrorista: %d\n\n\n", contador_rodadas_terrorista, contador_rodadas_contraterrorista);
   }
+}
+
+void planta_bomba(int id_terrorista)
+{
+  estado_bomba = BOMBA_PLANTADA;
+  printf("Terrorista %d esta plantando a bomba\n", id_terrorista);
+  sleep(15);
+}
+
+void desarma_bomba(int id_contraterrorista)
+{
+  estado_bomba = BOMBA_DESARMADA;
+  printf("ContraTerrorista %d esta desarmando a bomba\n", id_contraterrorista);
+  sleep(10);
 }
---
+++ CounterStrike/readme.md
@@ -0,0 +1,5 @@
+# Como compilar o counter strike:
+
+```bash
+  gcc -pthread -Wall -Ofast -o cs counterstrike.c
+```
